0aeac4089bde8c12e708816debfa6b0148a6ff5f
Brendon Harris <brendon.harris9@gmail.com>
2018-11-07 16:44:01 -0600

    more updates

--- frontend/app/controllers/sessions_controller.rb
+++ frontend/app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to 'http://localhost:4444/oauth2/auth?client_id=app&response_type=code&state=72582c142d403a972b579f05140bf93f&nonce=gyukgyglgghjjkk&redirect_uri=http://localhost:3000/sessions/callback&scope=openid+offline'
   end
 
   def callback
